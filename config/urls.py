fa74e6b074e7a6ddee1c84c0982b71f3fd8e6848
COnspirer <shreylol321@gmail.com>
2026-02-26 18:35:00 +0530

    Added home page

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Healthcare API is running",
+        "status": "ok"
+    })
 
 urlpatterns = [
+    path('', home)
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/patients/', include('patients.urls')),
